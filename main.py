512303ad6ce3980fcc238d4056aa9e8d9343637a
turfe <malevannyi.vm@phystech.edu>
2021-04-04 20:11:07 +0300

    added bilinear interpolation, rewrite functions for pattern of Bayer filter on Canon 600D, calculate exposure value from EXIF-data

--- main.py
+++ main.py
@@ -3,45 +3,130 @@ import numpy as np
 import rawpy
 import imageio
 import matplotlib.pyplot as plot
+import exifread
+import math
+
 
 def white_balance(image_array, matrix_wb):
     white_balance = np.zeros((2, 2), dtype=np.double)
-    white_balance[0][0] = matrix_wb[0] / matrix_wb[1]
-    white_balance[0][1] = white_balance[1][0] = matrix_wb[1] / matrix_wb[1]
-    white_balance[1][1] = matrix_wb[2] / matrix_wb[1]
+    white_balance[1][0] = matrix_wb[0] / matrix_wb[1]
+    white_balance[0][0] = white_balance[1][1] = matrix_wb[1] / matrix_wb[1]
+    white_balance[0][1] = matrix_wb[2] / matrix_wb[1]
     white_balance = np.tile(white_balance, (image_array.shape[0] // 2, image_array.shape[1] // 2))
     image_result = np.clip(image_array * white_balance, 0, 1)
     return image_result
 
 
+def convert_to_float(frac_str):
+    try:
+        return float(frac_str)
+    except ValueError:
+        try:
+            num, denom = frac_str.split('/')
+        except ValueError:
+            return None
+        try:
+            leading, num = num.split(' ')
+        except ValueError:
+            return float(num) / float(denom)
+        if float(leading) < 0:
+            sign_mult = -1
+        else:
+            sign_mult = 1
+        return float(leading) + sign_mult * (float(num) / float(denom))
+
+
 def demosaic_downsampling(image_array):
     image_result = np.empty((image_array.shape[0] // 2, image_array.shape[1] // 2, 3), dtype=np.double)
-    image_result[:, :, 0] = image_array[0::2, 0::2]
-    image_result[:, :, 1] = (image_array[0::2, 1::2] + image_array[1::2, 0::2]) / 2
-    image_result[:, :, 2] = image_array[1::2, 1::2]
+    image_result[:, :, 0] = image_array[1::2, 0::2]
+    image_result[:, :, 1] = (image_array[0::2, 0::2] + image_array[1::2, 1::2]) / 2
+    image_result[:, :, 2] = image_array[0::2, 1::2]
     return image_result
 
 
-def transfrom_to_sRGB(image_array, matrix_to_sRGB):
+def edge_interpolation(image_array, image_output, i, j, threshold, color):
+    if color == 1:
+        if abs(image_array[i - 1][j] - image_array[i + 1][j]) < threshold and \
+                abs(image_array[i][j - 1] - image_array[i + 1][j]) < threshold:
+            return (image_array[i - 1][j] + image_array[i + 1][j] +
+                    image_array[i][j - 1] + image_array[i][j + 1]) / 4
+        elif abs(image_array[i - 1][j] - image_array[i + 1][j]) < threshold:
+            return (image_array[i][j - 1] + image_array[i][j + 1]) / 2
+        else:
+            return (image_array[i - 1][j] + image_array[i + 1][j]) / 2
+    else:
+        if abs(image_array[i - 1][j - 1] - image_array[i + 1][j + 1]) < threshold and \
+                abs(image_array[i + 1][j - 1] - image_array[i - 1][j + 1]) < threshold:
+            return (image_array[i - 1][j - 1] + image_array[i + 1][j + 1] +
+                    image_array[i + 1][j - 1] + image_array[i - 11][j + 1]) / 4
+        elif abs(image_array[i - 1][j - 1] - image_array[i + 1][j + 1]) < threshold:
+            return (image_array[i + 1][j - 1] + image_array[i - 1][j + 1]) / 2
+        else:
+            return (image_array[i - 1][j - 1] + image_array[i + 1][j + 1]) / 2
+
+
+def demosaic(image_array):
+    image_result = np.zeros((image_array.shape[0], image_array.shape[1], 3), dtype=np.double)
+    map_filter = np.zeros((image_array.shape[0], image_array.shape[1]), dtype=np.uint8)
+    map_filter[1::2, 0::2] = 0  # Red
+    map_filter[0::2, 1::2] = 2  # Blue
+    map_filter[0::2, 0::2] = map_filter[1::2, 1::2] = 1  # Green
+
+    image_result[0::2, 0::2, 1] = image_array[0::2, 0::2]
+    image_result[1::2, 1::2, 1] = image_array[1::2, 1::2]
+    image_result[0::2, 1::2, 2] = image_array[0::2, 1::2]
+    image_result[1::2, 0::2, 0] = image_array[1::2, 0::2]
+
+    for i in range(1, image_result.shape[0] - 1):
+        for j in range(1, image_result.shape[1] - 1):
+            if map_filter[i][j] == 0:
+                image_result[i][j][1] = edge_interpolation(image_array, image_result, i, j, 1, 1)
+                image_result[i][j][2] = edge_interpolation(image_array, image_result, i, j, 1, 2)
+            elif map_filter[i][j] == 2:
+                image_result[i][j][1] = edge_interpolation(image_array, image_result, i, j, 1, 1)
+                image_result[i][j][0] = edge_interpolation(image_array, image_result, i, j, 1, 0)
+            else:
+                if map_filter[i - 1][j] == 2:
+                    image_result[i][j][0] = (image_array[i][j - 1] + image_array[i][j + 1]) / 2
+                    image_result[i][j][2] = (image_array[i - 1][j] + image_array[i + 1][j]) / 2
+                else:
+                    image_result[i][j][2] = (image_array[i][j - 1] + image_array[i][j + 1]) / 2
+                    image_result[i][j][0] = (image_array[i - 1][j] + image_array[i + 1][j]) / 2
+
+    return image_result
+
+
+def transform_to(image_array, transform_matrix):
     image_result = np.empty((image_array.shape[0], image_array.shape[1], image_array.shape[2]), dtype=np.double)
-    image_result[:, :, 0] = image_array[:, :, 0] * matrix_to_sRGB[0][0] + image_array[:, :, 1] * matrix_to_sRGB[0][1] + \
-                            image_array[:, :, 2] * matrix_to_sRGB[0][2]
-    image_result[:, :, 1] = image_array[:, :, 0] * matrix_to_sRGB[1][0] + image_array[:, :, 1] * matrix_to_sRGB[1][1] + \
-                            image_array[:, :, 2] * matrix_to_sRGB[1][2]
-    image_result[:, :, 2] = image_array[:, :, 0] * matrix_to_sRGB[2][0] + image_array[:, :, 1] * matrix_to_sRGB[2][1] + \
-                            image_array[:, :, 2] * matrix_to_sRGB[2][2]
+    image_result[:, :, 0] = image_array[:, :, 0] * transform_matrix[0][0] + image_array[:, :, 1] * transform_matrix[0][1] + \
+                            image_array[:, :, 2] * transform_matrix[0][2]
+    image_result[:, :, 1] = image_array[:, :, 0] * transform_matrix[1][0] + image_array[:, :, 1] * transform_matrix[1][1] + \
+                            image_array[:, :, 2] * transform_matrix[1][2]
+    image_result[:, :, 2] = image_array[:, :, 0] * transform_matrix[2][0] + image_array[:, :, 1] * transform_matrix[2][1] + \
+                            image_array[:, :, 2] * transform_matrix[2][2]
     return image_result
 
 
 path = 'example.CR2'
+
+with open(path, 'rb') as raw_file:
+    tags = exifread.process_file(raw_file)
+    f_number = convert_to_float(str(dict(tags.items()).get('EXIF FNumber')))
+    exp_time = convert_to_float(str(dict(tags.items()).get('EXIF ExposureTime')))
+    print(f'f-number:                     {f_number}')
+    print(f'exposure time:                {exp_time}')
+    exp_value = math.log((math.pow(f_number, 2) / exp_time), 2)
+    print(f'exposure value:               {exp_value}')
+
 with rawpy.imread(path) as raw:
     postprocess_rawpy = raw.postprocess()
     print(f'number of colors:             {raw.num_colors}')
+    print(f'raw patern:                   {raw.raw_pattern.tolist()}')
     print(f'color description:            {raw.color_desc}')
     print(f'white level:                  {raw.white_level}')
     print('XYZ to RGB conversion matrix:')
     print(raw.rgb_xyz_matrix)
-    print(f'camera white balance:         {raw.camera_whitebalance}')
+    print(f'device white balance:         {raw.camera_whitebalance}')
 
     raw_image = raw.raw_image_visible
     black_level = np.min(raw_image)
@@ -51,7 +136,7 @@ with rawpy.imread(path) as raw:
     device_wb = raw.camera_whitebalance
     image_wb = white_balance(raw_image, device_wb)
 
-    image_demosaic = demosaic_downsampling(image_wb)
+    image_demosaic = demosaic(image_wb)
 
     matrix_XYZ_to_device = np.array(raw.rgb_xyz_matrix[0:3, 0:3], dtype=np.double)
     matrix_sRGB_to_XYZ = np.array([[0.4124564, 0.3575761, 0.1804375],
@@ -62,7 +147,7 @@ with rawpy.imread(path) as raw:
     matrix_sRGB_to_device = matrix_sRGB_to_device / normalization
     matrix_device_to_sRGB = np.linalg.inv(matrix_sRGB_to_device)
 
-    image_sRGB = transfrom_to_sRGB(image_demosaic, matrix_device_to_sRGB)
+    image_sRGB = transform_to(image_demosaic, matrix_device_to_sRGB)
 
     image_sRGB = np.clip(image_sRGB, 0, 1)
     image_sRGB = image_sRGB ** (1 / 2.2)
